814b6f373c06a620c3dd0ef78d7893d6f2aa39dd
Otávio Albuquerque <otavioalbuquerque@Otavios-MacBook-Pro.local>
2025-07-03 20:23:42 -0300

    novas mudanças

--- Sources/Services/PokemonAPIService.swift
+++ Sources/Services/PokemonAPIService.swift
@@ -2,13 +2,13 @@ import Foundation
 
 struct PokemonAPIService {
 
-    private let baseURL = "https://pokeapi.co/api/v2/pokemon/"
+    private let baseURL = "https://pokeapi.co/api/v2/pokemon"
 
     func fetchPokemon(by name: String) async throws -> Pokemon {
 
         // GENGAR brabo -> gengar brabo -> gengarbrabo
         let nomeFormatado = name.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        guard let url = URL(string: "\(baseURL)\(nomeFormatado)") else {
+        guard let url = URL(string: "\(baseURL)/\(nomeFormatado)") else {
             throw APIError.invalidURL
         }
 
@@ -21,6 +21,27 @@ struct PokemonAPIService {
         do {
             let pokemon = try JSONDecoder().decode(Pokemon.self, from: data)
             return pokemon
+        } catch {
+            throw APIError.decodingError(error)
+        }
+
+    }
+
+    func fetchPokemonList(limit: Int = 20, offset: Int = 0) async throws -> PokemonList {
+
+        guard let url = URL(string: "\(baseURL)?limit=\(limit)&offset=\(offset)") else {
+            throw APIError.invalidURL
+        }
+
+        let (data, response) = try await URLSession.shared.data(from: url)
+
+        if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 404 {
+            throw APIError.notFound
+        }
+
+        do {
+            let pokemonList = try JSONDecoder().decode(PokemonList.self, from: data)
+            return pokemonList
         } catch {
             throw APIError.decodingError(error)
         }
